c45a5ca848628d609ed2559ec11b1e35174bddc4
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-02 13:11:41 +0100

    have employee data and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 function App() {
 	const title = 'Employee Directory';
+
+	const introText = "Welcome to our Employee Directory, your one-stop destination for discovering and connecting with the talented individuals who make our organization thrive.";
+
 	return (
 		<>
 			<h1 className="text-3xl mb-3">{title}</h1>
-			<p>test</p>
+			<p>{introText}</p>
 		</>
 	);
 }
